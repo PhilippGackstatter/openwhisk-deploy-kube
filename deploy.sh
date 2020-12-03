e3b616ee6b6a8dc5ab9d30fc149938b320b158df
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2020-12-03 12:01:21 +0100

    Add script to automate kind deployment

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+DEPLOY_BRANCH=`git rev-parse --abbrev-ref HEAD`
+OW_BRANCH=`git --git-dir=../openwhisk/.git rev-parse --abbrev-ref HEAD`
+
+echo "openwhisk-deploy-kube HEAD is at $DEPLOY_BRANCH and openwhisk is at $OW_BRANCH."
+
+if [[ "$(read -e -p 'Continue? [y/N]> '; echo $REPLY)" == [Nn]* ]]
+then
+    exit
+fi
+
+kind create cluster --config kind-cluster.yaml
+INTERNALIP=`kubectl describe node kind-worker | grep InternalIP: | awk '{print $2}'`
+yq -y .whisk.ingress.apiHostName=\"$INTERNALIP\" cluster-template.yaml > mycluster.yaml
+
+kubectl label node kind-worker openwhisk-role=core
+kubectl label node kind-worker2 openwhisk-role=invoker
+
+# These images are built locally
+kind load docker-image --nodes kind-worker,kind-worker2 whisk/invoker:latest
+kind load docker-image --nodes kind-worker,kind-worker2 whisk/controller:latest
+
+# These images are pulled from a registry
+kind load docker-image --nodes kind-worker,kind-worker2 openwhisk/apigateway
+kind load docker-image --nodes kind-worker,kind-worker2 apache/couchdb
+# kind load docker-image --nodes kind-worker,kind-worker2 openwhisk/controller
+# kind load docker-image --nodes kind-worker,kind-worker2 openwhisk/invoker
+kind load docker-image --nodes kind-worker,kind-worker2 wurstmeister/kafka
+kind load docker-image --nodes kind-worker,kind-worker2 openwhisk/ow-utils
+kind load docker-image --nodes kind-worker,kind-worker2 zookeeper
+kind load docker-image --nodes kind-worker,kind-worker2 nginx
+kind load docker-image --nodes kind-worker,kind-worker2 redis
+kind load docker-image --nodes kind-worker,kind-worker2 busybox
+kind load docker-image --nodes kind-worker,kind-worker2 openwhisk/alarmprovider
+kind load docker-image --nodes kind-worker,kind-worker2 openwhisk/kafkaprovider
+
+helm install owdev ./helm/openwhisk -n openwhisk --create-namespace -f mycluster.yaml
+
+wsk property set --apihost 127.0.0.1:31001
---
+++ pull_docker_images.sh
@@ -0,0 +1,11 @@
+docker pull openwhisk/apigateway
+docker pull apache/couchdb
+docker pull openwhisk/controller
+docker pull wurstmeister/kafka
+docker pull openwhisk/ow-utils
+docker pull zookeeper
+docker pull nginx
+docker pull redis
+docker pull busybox
+docker pull openwhisk/alarmprovider
+docker pull openwhisk/kafkaprovider
